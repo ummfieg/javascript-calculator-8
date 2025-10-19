110b28893ada6b992589b7b71b5113774c5f9a6f
ummfieg <ummfieg@naver.com>
2025-10-19 22:59:35 +0900

    feat(app): 예외처리 (커스텀 구분자 형식이 아닐경우)추가 및 구분자 전역스코프로 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const DEFAULT_DELIM = /[,:]/;
+const CUSTOM_DELIM = /\/\/(.*?)\n/;
+
 class App {
   async run() {
     try {
@@ -7,8 +10,10 @@ class App {
         "덧셈할 문자열을 입력해주세요.\n"
       );
       console.log("입력한 값:", userInput);
+      this.isValidInput(userInput);
       this.calculator(userInput);
     } catch (error) {
+      this.errorMessage(error);
       throw error;
     }
   }
@@ -16,8 +21,6 @@ class App {
     let result = 0;
     let splitText;
     let convertNum;
-    const DEFALUT_DELIM = /[,:]/;
-    const CUSTOM_DELIM = /\/\/(.*?)\n/;
     let inputEnter = userInput.replace(/\\n/g, "\n");
 
     //커스텀 구분자
@@ -27,8 +30,8 @@ class App {
       splitText = inputEnter.replace(CUSTOM_DELIM, "").split(userDelim);
     }
     //기본 구분자
-    else if (DEFALUT_DELIM.test(userInput)) {
-      splitText = userInput.split(DEFALUT_DELIM);
+    else if (DEFAULT_DELIM.test(userInput)) {
+      splitText = userInput.split(DEFAULT_DELIM);
     }
     //빈문자열
     else if (userInput === " ") {
@@ -41,6 +44,23 @@ class App {
     convertNum = splitText.map((num) => Number(num));
     result = convertNum.reduce((total, num) => total + num, 0);
     Console.print(`결과 : ${result}`);
+  }
+
+  isValidInput(userInput) {
+    const inputEnter = userInput.replace(/\\n/g, "\n");
+
+    // 1. 커스텀 구분자 검증
+    if (CUSTOM_DELIM.test(inputEnter)) {
+      let userDelim = inputEnter.match(CUSTOM_DELIM)[1];
+      if (!userDelim) {
+        throw new Error(`[ERROR] 커스텀 구분자 형식이 잘못되었습니다.`);
+      }
+    }
+    return userInput;
+  }
+
+  errorMessage(error) {
+    Console.print(error.message);
   }
 }
 export default App;
