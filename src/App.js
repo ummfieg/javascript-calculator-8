e6b374b4d9e801c6d1ba3bdad4bacab49784a261
ummfieg <ummfieg@naver.com>
2025-10-19 17:00:22 +0900

    feat(app): 문자열 입력 및 구분자 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-class App {
-  async run() {}
-}
+import { Console } from "@woowacourse/mission-utils";
 
+class App {
+  async run() {
+    try {
+      let userInput = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해주세요.\n"
+      );
+      console.log("입력한 값:", userInput);
+      this.calculator(userInput);
+    } catch (error) {
+      throw error;
+    }
+  }
+  calculator(userInput) {
+    let result = 0;
+    let splitText;
+    let convertNum;
+    const DEFALUT_DELIM = /[,:]/;
+    const CUSTOM_DELIM = /\/\/(.*?)\n/;
+    let inputEnter = userInput.replace(/\\n/g, "\n");
+  }
+}
 export default App;
