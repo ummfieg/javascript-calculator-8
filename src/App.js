f1ec732a3ab8406117574eb76f3ec1f000ca1ec1
ummfieg <ummfieg@naver.com>
2025-10-20 17:18:35 +0900

    refactor(app): 계산 및 검증 함수 중복 로직 수정

--- src/App.js
+++ src/App.js
@@ -1,55 +1,38 @@
 import { Console } from "@woowacourse/mission-utils";
 
-const DEFAULT_DELIM = /[,:]/;
-const CUSTOM_DELIM = /\/\/(.*?)\n/;
-
 class App {
   async run() {
     try {
-      let userInput = await Console.readLineAsync(
+      const USER_INPUT = await Console.readLineAsync(
         "덧셈할 문자열을 입력해주세요.\n"
       );
-      if (userInput.trim() === "") {
+      if (USER_INPUT.trim() === "") {
         Console.print(0);
         return 0;
       }
-      const INPUT_ENTER = userInput.replace(/\\n/g, "\n");
-      this.processInput(INPUT_ENTER);
-      this.calculator(INPUT_ENTER);
+      const INPUT_ENTER = USER_INPUT.replace(/\\n/g, "\n");
+      const VALIDATED_NUMBERS = this.processInput(INPUT_ENTER);
+      this.calculator(VALIDATED_NUMBERS);
     } catch (error) {
       this.errorMessage(error);
       throw error;
     }
   }
-  calculator(INPUT_ENTER) {
-    let splitText;
-
-    //커스텀 구분자
-    if (CUSTOM_DELIM.test(INPUT_ENTER)) {
-      let isMatchDelim = INPUT_ENTER.match(CUSTOM_DELIM);
-      userDelim = isMatchDelim[1];
-      splitText = INPUT_ENTER.replace(CUSTOM_DELIM, "").split(userDelim);
-    }
-    //기본 구분자
-    else if (DEFAULT_DELIM.test(INPUT_ENTER)) {
-      splitText = INPUT_ENTER.split(DEFAULT_DELIM);
-    }
-
-    let convertNum = splitText.map((num) => Number(num));
-    let result = convertNum.reduce((total, num) => total + num, 0);
-    Console.print(`결과 : ${result}`);
-  }
-
   processInput(INPUT_ENTER) {
+    const DEFAULT_DELIM = /[,:]/;
+    const CUSTOM_DELIM = /\/\/(.*?)\n/;
     const HAS_CUSTOM_DELIM = CUSTOM_DELIM.test(INPUT_ENTER);
+    let numbers = [];
 
     // 1. 커스텀 구분자 검증
+    let parsingDelim;
     if (HAS_CUSTOM_DELIM) {
-      let isMatchDelim = INPUT_ENTER.match(CUSTOM_DELIM)[1];
-      if (!isMatchDelim) {
+      parsingDelim = INPUT_ENTER.match(CUSTOM_DELIM)[1];
+      if (!parsingDelim) {
         throw new Error(`[ERROR] 커스텀 구분자 형식이 잘못되었습니다.`);
       }
     }
+
     // 2. 기본 구분자 검증
     const VALID_DEFAULT = /^[0-9a-zA-Z,:-]+$/;
     if (!HAS_CUSTOM_DELIM && !VALID_DEFAULT.test(INPUT_ENTER)) {
@@ -57,9 +40,7 @@ class App {
     }
 
     // 3. 숫자 검증
-    let numbers = [];
     if (HAS_CUSTOM_DELIM) {
-      let parsingDelim = INPUT_ENTER.match(CUSTOM_DELIM)[1];
       numbers = INPUT_ENTER.replace(CUSTOM_DELIM, "")
         .split(parsingDelim)
         .map(Number);
@@ -76,8 +57,14 @@ class App {
       throw new Error(`[ERROR] 음수는 입력할 수 없습니다.`);
     }
 
-    return INPUT_ENTER;
+    return numbers;
   }
+
+  calculator(VALIDATED_NUMBERS) {
+    let result = VALIDATED_NUMBERS.reduce((total, num) => total + num, 0);
+    Console.print(`결과 : ${result}`);
+  }
+
   errorMessage(error) {
     Console.print(error.message);
   }
